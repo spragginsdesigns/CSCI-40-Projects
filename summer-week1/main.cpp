79b4b8a33d47e6b7a8c72f65a0852d8b74607f4f
Austin Spraggins <atmosphere9999@gmail.com>
2024-06-18 10:15:49 -0700

    Project #1: Tell Me a Joke - Finished

---
+++ summer-week1/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Project Title: Tell Me A Joke
+ * Objective: Print a joke to the screen.
+ * Author: Austin Spraggins
+ * Student ID: 0524626
+ * Date: 06/18/2024
+ * Due Date: 06/23/2024
+ * Instructor: Brian Baker
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "What happens when you buy Nacho Cheese?\nIt becomes Cho Cheese!" << std::endl;
+  return 0;
+}
